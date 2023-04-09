f385c0b9e448ef781f7c4008666f0c0d1503899e
Akshay Sharma <akshaya.sharma770@gmail.com>
2023-04-09 21:29:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var http = require('http');
 
-console.log(`Nodejs started on 8080 top`);
+console.log(`Nodejs started on 5000 top`);
 //create a server object:
 http.createServer(function (req, res) {
-  console.log(`Nodejs started on 8080 top inner`);
+  console.log(`Nodejs started on 5000 top inner`);
   res.write('Hello World!'); //write a response to the client
   res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+}).listen(5000); //the server object listens on port 8080
