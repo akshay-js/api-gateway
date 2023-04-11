9022aa32fa334b5d36d468c0a066da4b64feb6e9
Akshay Sharma <akshaya.sharma770@gmail.com>
2023-04-11 21:50:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.get('/azure-aks', async (req, res) => {
   console.log('Query string', req.query.url);
    try {
     const response = await axios.get(`${req.query.url}`);
-    console.log(response);
-     return response;
+    console.log(response.data);
+     res.send(response.data);
   } catch (error) {
     console.error(error);
-    return error;
+    res.send(error);
   }
 });
 
