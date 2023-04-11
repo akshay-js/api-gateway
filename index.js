94d49bc80a11ac4184145616741ec23504a2103f
Akshay Sharma <akshaya.sharma770@gmail.com>
2023-04-11 21:10:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 5000;
 
 app.get('/hello-world', (req, res) => {
-  res.send('Hello World! from API Gateway service')
-})
+  res.send('Hello World! from API Gateway service');
+});
+
+app.get('/azure-aks', async (req, res) => {
+  console.log('Query string', req.query.url);
+   try {
+    const response = await axios.get(`${req.query.url}`);
+    console.log(response);
+     return response;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+});
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
